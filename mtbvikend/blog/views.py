b82486bf8f885292f4fb1776517ef67936c090a4
salasy <sasa.kraljevic@gmail.com>
2017-06-25 15:32:56 +0200

    basic blog functions

--- mtbvikend/blog/views.py
+++ mtbvikend/blog/views.py
@@ -9,10 +9,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request):
+def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
+'''
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -41,3 +41,4 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form: form'})
+'''
